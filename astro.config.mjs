82dbf1e4ee296d460538525fb7a641085b466dd7
Alexander Hack <15661662+signet-marigold@users.noreply.github.com>
2023-10-26 19:34:35 +0000

    added todo.txt and added tailwind css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import node from "@astrojs/node";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://averydavis.me',
   output: "server",
   adapter: node({
     mode: "standalone"
-  })
+  }),
+  integrations: [tailwind()]
 });
