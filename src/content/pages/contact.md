6ea8621194f8cdb92e9ebd77b809dbc48c3b85ea
Dan <contact+github@danmyers.net>
2023-12-03 10:39:36 -0600

    merged with dev

---
+++ src/content/pages/contact.md
@@ -0,0 +1,9 @@
+---
+title: Get in touch with me!
+---
+
+_Email:_
+Feel free to drop me an email at [averydavistutor@gmail.com](mailto:averydavistutor@gmail.com), and I'll do my best to respond as soon as possible.
+
+Or text me at
+512.568.5151
---
+++ src/data/site-config.ts
@@ -0,0 +1,90 @@
+export type Image = {
+    src: string;
+    alt?: string;
+};
+
+export type Link = {
+    href: string;
+    text: string;
+};
+
+export type SocialLink = Link & {
+    icon: 'codepen' | 'dev' | 'facebook' | 'github' | 'instagram' | 'linkedin' | 'medium' | 'x-twitter';
+};
+
+export type Hero = {
+    title?: string;
+    text?: string;
+    avatar?: Image;
+    backgroundImage?: Image;
+};
+
+export type SiteConfig = {
+    logo?: Image;
+    title: string;
+    description: string;
+    image?: Image;
+    primaryNavLinks?: Link[];
+    secondaryNavLinks?: Link[];
+    socialLinks?: SocialLink[];
+    hero?: Hero;
+    postsPerPage?: number;
+};
+
+const siteConfig: SiteConfig = {
+    logo: {
+        src: '/signature.svg',
+        alt: 'Avery Logo'
+    },
+    title: 'Avery Davis',
+    description: 'Avery Davis Tutoring',
+    image: {
+        src: '/hero.jpg',
+        alt: 'Avery Davis Tutoring'
+    },
+    primaryNavLinks: [
+        {
+            text: 'Home',
+            href: '/'
+        },
+        {
+            text: 'About',
+            href: '/about'
+        },
+        {
+            text: 'Contact',
+            href: '/contact'
+        },
+    ],
+    secondaryNavLinks: [
+        {
+            text: 'About',
+            href: '/about'
+        },
+        {
+            text: 'Contact',
+            href: '/contact'
+        }
+    ],
+    // socialLinks: [
+    //     {
+    //         text: 'Follow on Instagram',
+    //         href: 'https://instagram.com/',
+    //         icon: 'instagram'
+    //     },
+    // ],
+    hero: {
+        title: 'Hi there!',
+        text: "My name is Avery Davis! I'm a tutor and I love to help students learn.",
+        avatar: {
+            src: '/avatar.jpg',
+            alt: 'Avery Davis'
+        },
+        backgroundImage: {
+            src: '/hero.jpg'
+        }
+    },
+    postsPerPage: 5
+};
+
+export default siteConfig;
