24f00d223356d2eb5aacb56b4e0434e204b1ce22
Dan <contact+github@danmyers.net>
2023-12-04 10:56:36 -0600

    switched off ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,12 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://averydavis.me',
-  integrations: [mdx(), sitemap(), tailwind()],
-  output: "server",
-  adapter: vercel()
+    site: 'https://averydavis.me',
+    integrations: [mdx(), sitemap(), tailwind()],
+    output: 'hybrid',
+    adapter: vercel()
 });
