145803620b5c842664c6ce5f84d5057daf19819d
Dan <contact+github@danmyers.net>
2023-12-03 21:04:04 -0600

    fixed ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
-import vercelServerless from '@astrojs/vercel/serverless';
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://averydavis.me',
-    integrations: [mdx(), sitemap(), tailwind()],
-    output: 'server',
-    adapter: vercel(),
+  site: 'https://averydavis.me',
+  integrations: [mdx(), sitemap(), tailwind()],
+  output: "server",
+  adapter: vercel()
 });
