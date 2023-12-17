1aa892b68614cf1942d4658f386f61e0d315138f
Dan <contact+github@danmyers.net>
2023-12-17 14:48:32 -0600

    working on express

--- api/serverless-example.ts
+++ api/serverless-example.ts
@@ -1,12 +1,55 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
+import { VercelRequest, VercelResponse } from '@vercel/node';
+import express from 'express';
 
-export default function handler(
-  request: VercelRequest,
-  response: VercelResponse,
-) {
-  response.status(200).json({
-    body: request.body,
-    query: request.query,
-    cookies: request.cookies,
-  });
+const app = express();
+app.use(express.json());
+
+app.post('/register', (req, res) => {
+  // Assuming the request body contains user registration data
+  const { username, email, password } = req.body;
+
+  // Perform validation and error checking here
+
+  // Assuming you have a function to save user data to a database
+  // You should use a secure password hashing mechanism (e.g., bcrypt)
+  const userId = saveUserDataToDatabase(username, email, password);
+
+  res.status(201).json({ userId, username, email });
+});
+
+// Helper function to save user data to a database (you would need to implement this)
+function saveUserDataToDatabase(username, email, password) {
+  // In a real-world scenario, save this data securely to a database
+  // For simplicity, let's just return a placeholder user ID
+  const userId = Math.floor(Math.random() * 1000) + 1;
+  return userId;
 }
+
+// Example of a route to get user information by ID
+app.get('/user/:id', (req, res) => {
+  const userId = req.params.id;
+
+  // Assuming you have a function to retrieve user data from the database
+  const userData = getUserDataFromDatabase(userId);
+
+  if (userData) {
+    res.status(200).json(userData);
+  } else {
+    res.status(404).json({ error: 'User not found' });
+  }
+});
+
+// Helper function to retrieve user data from a database (you would need to implement this)
+function getUserDataFromDatabase(userId) {
+  // In a real-world scenario, retrieve user data from a database
+  // For simplicity, let's return null for non-existent users
+  return null;
+}
+
+// Set up your server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+export default app;
