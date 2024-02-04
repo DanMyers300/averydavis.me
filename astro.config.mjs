0d3a12b9d7faeef66fbc18d75f4864bd7050ca92
Dan Myers <106322207+DanMyers300@users.noreply.github.com>
2024-02-04 23:02:08 +0000

    added carousel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,14 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-
 import vercelServerless from '@astrojs/vercel/serverless';
+
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://averydavis.me',
-    integrations: [mdx(), sitemap(), tailwind(), react()],
-    output: 'server',
-    adapter: vercelServerless(),
+  site: 'https://averydavis.me',
+  integrations: [mdx(), sitemap(), tailwind(), react(), solidJs()],
+  output: 'server',
+  adapter: vercelServerless()
 });
